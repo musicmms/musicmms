8802d47062feac95049ad00c80e9481021f415bf
Ronit Sharma <sharmaronit009@gmail.com>
2017-11-04 20:17:21 -0700

    ahhhhh

--- controllers/messageHandlers.js
+++ controllers/messageHandlers.js
@@ -1,7 +1,19 @@
-require('dotenv').config(); const ffmpeg = require('ffmpeg'); const accountSid = process.env.TWILIO_ACCOUNT; const authToken = 
-process.env.TWILIO_AUTH; const search = require('youtube-search'); // require the Twilio module and create a REST client const 
-client = require('twilio')(accountSid, authToken); const MessagingResponse = require('twilio').twiml.MessagingResponse; const 
-helpers = require('../helpers'); exports.sendmsg = (req, res) => {
+require('dotenv').config();
+const ffmpeg = require('ffmpeg');
+const accountSid = process.env.TWILIO_ACCOUNT;
+const authToken = process.env.TWILIO_AUTH;
+const search = require('youtube-search'); // require the Twilio module and create a REST client 
+const client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const helpers = require('../helpers');
+const { YTSearcher } = require('ytsearcher');
+const searcher = new YTSearcher('AIzaSyALc4i5Kng8dxGwU9JKCNu7PKIjXwXw6ZQ')
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const uuidv4 = require('uuid/v4');
+const path = require('path');
+
+exports.sendmsg = (req, res) => {
     let body = req.query.body;
     let number = parseInt(req.query.number);
     if (!(req.query.number) || req.query.number.length < 10) {
@@ -9,7 +21,7 @@ helpers = require('../helpers'); exports.sendmsg = (req, res) => {
             error: true,
             errormsg: "Number is not correct length, not sent, or you included 1 at the beginning, which we do not like."
         })
-    } 
+    }
     // else {
     // res.send({
     // error: false,
@@ -25,16 +37,32 @@ helpers = require('../helpers'); exports.sendmsg = (req, res) => {
         })
         .then((message) => res.send(`Sent message with id ${message.sid}`));
 }
+
 exports.receivemsg = (req, res) => {
     console.log("Message received!");
     const twiml = new MessagingResponse();
-    const opts = {
-        maxResults: 10,
-        key: 'AIzaSyALc4i5Kng8dxGwU9JKCNu7PKIjXwXw6ZQ'
-    };
-    search(req.body.Body, opts, (err, results) => {
-        if(err) console.log(err);
-        helpers.downloadVideo(results[0].link);
-        console.log(results[0])
-    })
+    const searchTerm = req.body.Body;
+    let result = await searcher.search(searchTerm, { type: 'video' });
+    let videoLink = result.first.url;
+    let videoName = uuidv4();
+    ytdl(videoLink).pipe(fs.createWriteStream('../res/video/' + videoName + '.mp4'));
+    try {
+        var process = new ffmpeg('../res/video/' + videoName + '.mp4');
+        process.then(function (video) {
+            // Callback mode
+            video.fnExtractSoundToMP3('../res/audio/' + videoName + '.mp3', function (error, file) {
+                if (!error) console.log('Audio file: ' + file);
+            });
+        }, function (err) {
+            console.log('Error: ' + err);
+        }).then(function () {
+            fs.unlink('../res/video/' + videoName + '.mp4')
+        });
+    } catch (e) {
+        console.log(e.code);
+        console.log(e.msg);
+    }
+    if (path.exists('../res/audio/' + videoName + '.mp3')) {
+        console.log("IT WORKED !")
+    }
 }
