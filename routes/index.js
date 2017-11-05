3e6534942ed43242255bac096cc5e6a4cea03e4e
Ronit Sharma <sharmaronit009@gmail.com>
2017-11-05 08:21:40 -0800

    fixed call/text support

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,14 @@ router.get('/', (req, res) => {
 
 router.get('/sendmsg', messageHandlers.sendmsg);
 
-router.post('/sms', messageHandlers.receivemsg);
+router.post('/sms', (req, res) => {
+  if(req.body.Body.includes('call')) {
+    messageHandlers.receivemsgCall(req, res);
+  }
+  else if (req.body.Body.includes('text')) {
+    messageHandlers.receivemsgText(req, res);
+  }
+});
 
 router.get('/song/:song', (req, res) => {
   res.setHeader('Content-Type', 'audio/mpeg');
