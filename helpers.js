4eeba1bcb47ff58b41dfbd1ef77f0b790b073b4b
Ronit Sharma <sharmaronit009@gmail.com>
2017-11-04 19:18:05 -0700

    hi

--- helpers.js
+++ helpers.js
@@ -9,9 +9,10 @@ exports.downloadVideo = (vlink) => {
 
     video.on('info', function(info) {
         console.log('Download started');
-        console.log('filename: ' + info.filename);
+        console.log('filename: ' + info._filename);
         console.log('size: ' + info.size);
+        video.pipe(fs.createWriteStream('/res/video/' + info.filename + '.mp4'));
     });
-    video.pipe(fs.createWriteStream('/res/video/' + info.filename + '.mp4')); 
+     
 }
 
