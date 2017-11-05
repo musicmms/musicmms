6ee374b7aca89c3074c0f2596455ed0ca7c88440
Shaunak Kale <epicshaunak@gmail.com>
2017-11-05 06:52:20 -0800

    hi

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,9 @@ router.get('/sendmsg', messageHandlers.sendmsg);
 router.post('/sms', messageHandlers.receivemsg);
 
 router.get('/song/:song', (req, res) => {
-  res.setHeader('content-type', 'audio/mpeg');
-  res.sendFile('/app/res/audio/' + req.params.song)
-})
-router.get('/xml', (req, res) => {
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.sendFile('/app/res/audios/' + req.params.song)
+})router.get('/xml', (req, res) => {
   res.sendFile('/app/music.xml')
 
-})
-
-module.exports = router;
+})module.exports = router;
