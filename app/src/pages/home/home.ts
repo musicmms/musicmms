86b40b7c6f527e24e847dbd14fe5b431b7c9130e
Shaunak Kale <epicshaunak@gmail.com>
2017-11-04 17:39:46 -0700

    hi

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SMS } from '@ionic-native/sms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  songName: any;
+  constructor(public navCtrl: NavController, private sms: SMS) {
 
-  constructor(public navCtrl: NavController) {
 
+  }
+  sendMusic() {
+    console.log(this.songName)
+    var options = {
+      replaceLineBreaks: false, // true to replace \n by a new line, false by default
+
+    }
+    this.sms.send('4086769926', this.songName, options)
+      .then(() => {
+        alert("success");
+      }, () => {
+        alert("failed");
+      });
   }
 
 }
